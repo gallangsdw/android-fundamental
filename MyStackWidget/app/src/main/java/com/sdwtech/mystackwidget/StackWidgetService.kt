5dd89f5b9b932559ff0aba11b137b4740a3ca99e
gallang sadewa <gallang1800016063@webmail.uad.ac.id>
2021-04-08 22:41:16 +0700

    no message

---
+++ MyStackWidget/app/src/main/java/com/sdwtech/mystackwidget/StackWidgetService.kt
@@ -0,0 +1,9 @@
+package com.sdwtech.mystackwidget
+
+import android.content.Intent
+import android.widget.RemoteViewsService
+
+class StackWidgetService: RemoteViewsService() {
+    override fun onGetViewFactory(p0: Intent?): RemoteViewsFactory =
+        StackRemoteViewsFactory(this.applicationContext)
+}
