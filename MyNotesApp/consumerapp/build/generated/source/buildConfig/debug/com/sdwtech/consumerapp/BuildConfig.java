f951f3749690eedd3195cc45a4633bd00ad4059f
gallang sadewa <gallang1800016063@webmail.uad.ac.id>
2021-04-08 15:22:08 +0700

    no message

---
+++ MyNotesApp/consumerapp/build/generated/source/buildConfig/debug/com/sdwtech/consumerapp/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.sdwtech.consumerapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.sdwtech.consumerapp";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
